b200df58ff5124fbd18477974dbaf412c875ceac
Jay <wenyangzheng669@gmail.com>
2025-05-03 22:18:43 +0800

    添加订单详情查看功能

--- sky-server/src/main/java/com/sky/controller/admin/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -5,11 +5,13 @@ import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.OrderService;
 import com.sky.vo.OrderStatisticsVO;
+import com.sky.vo.OrderVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,5 +47,11 @@ public class OrderController {
         return Result.success(orderStatisticsVO);
     }
 
+    @GetMapping("/details/{id}")
+    @ApiOperation("查看订单详情")
+    public Result<OrderVO> detail(@PathVariable("id") Long id){
+        OrderVO orderVO = orderService.detail(id);
+        return Result.success(orderVO);
+    }
 }
 
